17d90e4f08025b25fb0ec53bc794b33076b65623
HerrTxbias <16502676+HerrTxbias@users.noreply.github.com>
2021-03-07 00:11:14 +0100

    fix

--- database/migrations/2021_02_16_233641_create_incidents_table.php
+++ database/migrations/2021_02_16_233641_create_incidents_table.php
@@ -28,7 +28,6 @@ class CreateIncidentsTable extends Migration {
             $table->boolean('visibility')->default(0);
             $table->integer('user');
 			$table->dateTime('scheduled_at')->nullable();
-            $table->dateTime('end_at')->nullable();
             $table->timestamps();
 		});
 	}
