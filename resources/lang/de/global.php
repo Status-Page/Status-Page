659afcdb3369f91f2e052649b4c3dabb9808ce75
HerrTxbias <admin@herrtxbias.net>
2021-03-07 23:53:45 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (1 of 1 strings)
    
    Translation: Status Page/Global
    Translate-URL: http://translate.herrtxbias.net/projects/status-page/global/de/

--- resources/lang/de/global.php
+++ resources/lang/de/global.php
@@ -1,1 +1,18 @@
 <?php
+return [
+    /*
+ * Copyright (c) 2021 by HerrTxbias.
+ *
+ * Using / Editing this without my consent is not allowed.
+ */
+    /*
+    |--------------------------------------------------------------------------
+    | Global Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | Please do not edit these lines, as they are managed by our translation
+    | system. Thank you.
+    |
+    */
+    'abort' => 'Abbrechen',
+];
