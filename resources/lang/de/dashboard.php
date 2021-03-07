a53cf6e60fa85e81331ff9636adb453eaffc8652
HerrTxbias <admin@herrtxbias.net>
2021-03-07 23:52:40 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (6 of 6 strings)
    
    Translation: Status Page/Dashboard
    Translate-URL: http://translate.herrtxbias.net/projects/status-page/dashboard/de/

--- resources/lang/de/dashboard.php
+++ resources/lang/de/dashboard.php
@@ -1,1 +1,25 @@
 <?php
+return [
+    /*
+ * Copyright (c) 2021 by HerrTxbias.
+ *
+ * Using / Editing this without my consent is not allowed.
+ */
+    /*
+    |--------------------------------------------------------------------------
+    | Dashboard Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | Please do not edit these lines, as they are managed by our translation
+    | system. Thank you.
+    |
+    */
+    'title' => 'Dashboard',
+    'view_all' => 'Alle anzeigen',
+    'logged_in' => 'Sie sind eingeloggt!',
+    'quick_view' => [
+        'incidents' => 'Offene Vorfälle',
+        'maintenances' => 'Offene Wartungen',
+        'upcoming_maintenances' => 'Anstehende Wartungen',
+    ],
+];
