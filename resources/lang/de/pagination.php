325cfcf597ad145e16c46bc76ec43a9a2b48a614
HerrTxbias <admin@herrtxbias.net>
2021-03-08 17:40:59 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: Status Page/Pagination
    Translate-URL: http://translate.herrtxbias.net/projects/status-page/pagination/de/

--- resources/lang/de/pagination.php
+++ resources/lang/de/pagination.php
@@ -1,1 +1,20 @@
 <?php
+return [
+    /*
+ * Copyright (c) 2021 by HerrTxbias.
+ *
+ * Using / Editing this without my consent is not allowed.
+ */
+    /*
+    |--------------------------------------------------------------------------
+    | Pagination Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the paginator library to build
+    | the simple pagination links. You are free to change them to anything
+    | you want to customize your views to better match your application.
+    |
+    */
+    'previous' => '&laquo; Zurück',
+    'next' => 'Weiter &raquo;',
+];
