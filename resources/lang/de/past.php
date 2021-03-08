92627066d12efd9a2c51738354dde192a77a3de4
HerrTxbias <admin@herrtxbias.net>
2021-03-08 17:42:15 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (9 of 9 strings)
    
    Translation: Status Page/Past
    Translate-URL: http://translate.herrtxbias.net/projects/status-page/past/de/

--- resources/lang/de/past.php
+++ resources/lang/de/past.php
@@ -1,1 +1,34 @@
 <?php
+return [
+    'tables' => [
+        'head' => [
+            'title' => 'Titel',
+            'status' => 'Status',
+            'impact' => 'Einfluss',
+            'reporter' => 'Melder',
+        ],
+    ],
+    'incidents' => [
+        'title' => 'Vergangene Vorfälle',
+        'subtitle' => 'Alle Vorfälle, die in der Vergangenheit passiert sind.',
+    ],
+    'maintenances' => [
+        'title' => 'Vergangene Wartungen',
+        'subtitle' => 'Alle Wartungen, die in der Vergangenheit stattgefunden haben.',
+    ],
+    /*
+ * Copyright (c) 2021 by HerrTxbias.
+ *
+ * Using / Editing this without my consent is not allowed.
+ */
+    /*
+    |--------------------------------------------------------------------------
+    | Past Incident / Maintenance Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | Please do not edit these lines, as they are managed by our translation
+    | system. Thank you.
+    |
+    */
+    'title' => 'Vergangene Vorfälle / Wartungen',
+];
