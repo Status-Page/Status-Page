59a46da333e7a8ff4e1e6c151aac02f0247e1f88
HerrTxbias <admin@herrtxbias.net>
2021-03-08 00:09:51 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (29 of 29 strings)
    
    Translation: Status Page/Maintenances
    Translate-URL: http://translate.herrtxbias.net/projects/status-page/maintenances/de/

--- resources/lang/de/maintenances.php
+++ resources/lang/de/maintenances.php
@@ -6,6 +6,53 @@ return [
             'impact' => 'Einfluss',
             'status' => 'Status',
             'reporter' => 'Melder',
+            'scheduled_at' => 'Geplant um',
+            'end_at' => 'Ende um',
+        ],
+    ],
+    'new_maintenance' => [
+        'modal' => [
+            'maintenance_title' => 'Titel',
+            'visible' => 'Sichtbar',
+            'scheduled_at' => 'Startzeit',
+            'end_at' => 'Endzeit',
+            'end_hint' => 'Wenn Sie diese Wartung nicht automatisch beenden wollen, geben Sie hier keinen Wert an.',
+            'affected_components' => 'Betroffene Komponenten',
+            'message' => 'Nachricht',
+            'schedule_button' => 'Wartung Planen',
+            'title' => 'Wartung Planen',
+        ],
+        'button' => 'Wartung Planen',
+    ],
+    /*
+ * Copyright (c) 2021 by HerrTxbias.
+ *
+ * Using / Editing this without my consent is not allowed.
+ */
+    /*
+    |--------------------------------------------------------------------------
+    | Maintenance Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | Please do not edit these lines, as they are managed by our translation
+    | system. Thank you.
+    |
+    */
+    'title' => 'Wartungen',
+    'update_maintenance' => [
+        'button' => 'Aktualisieren',
+        'modal' => [
+            'title' => 'Wartung Aktualisieren',
+            'maintenance_title' => 'Titel',
+            'status' => 'Status',
+            'visible' => 'Sichtbar',
+            'scheduled_at' => 'Startzeit',
+            'scheduled_hint' => 'Geben Sie einen neuen Wert an, um den vorhandenen zu überschreiben. Wenn Sie den Wert unverändert lassen, wird er nicht aktualisiert.',
+            'end_at' => 'Endzeit',
+            'end_hint' => 'Geben Sie einen neuen Wert an, um den vorhandenen zu überschreiben. Wenn Sie den Wert unverändert lassen, wird er nicht aktualisiert.',
+            'affected_components' => 'Betroffene Komponenten',
+            'message' => 'Nachricht',
+            'update_button' => 'Update posten',
         ],
     ],
 ];
