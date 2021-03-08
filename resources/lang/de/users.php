0063b6162663917cde2cf76a9b2cfbe5220b0585
HerrTxbias <admin@herrtxbias.net>
2021-03-08 17:43:29 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (28 of 28 strings)
    
    Translation: Status Page/Users
    Translate-URL: http://translate.herrtxbias.net/projects/status-page/users/de/

--- resources/lang/de/users.php
+++ resources/lang/de/users.php
@@ -1,1 +1,61 @@
 <?php
+return [
+    'table' => [
+        'head' => [
+            'name' => 'Name',
+            'email' => 'E-Mail',
+            'deactivated' => 'Deaktiviert',
+            'role' => 'Rolle',
+        ],
+    ],
+    'new_user' => [
+        'button' => 'Benutzer hinzufügen',
+        'modal' => [
+            'name' => 'Name',
+            'email' => 'E-Mail',
+            'password' => 'Passwort',
+            'deactivated' => 'Deaktiviert',
+            'role' => 'Rolle',
+            'save_button' => 'Speichern',
+            'title' => 'Benutzer hinzufügen',
+        ],
+    ],
+    'update_user' => [
+        'button' => 'Update',
+        'modal' => [
+            'title' => 'Benutzer Aktualisieren',
+            'email_hint' => 'Nur lesen',
+            'password' => 'Passwort',
+            'password_hint' => 'Wird nur aktualisiert, wenn Sie einen Wert einstellen.',
+            'deactivated' => 'Deaktiviert',
+            'role' => 'Rolle',
+            'update_button' => 'Update',
+            'name' => 'Name',
+            'email' => 'E-Mail',
+        ],
+    ],
+    'delete_user' => [
+        'button' => 'Löschen',
+        'modal' => [
+            'title' => 'Benutzer löschen',
+            'text_r1' => 'Sind Sie sicher, dass Sie den Benutzer ":name" löschen wollen?',
+            'text_r2' => 'Dies kann nicht rückgängig gemacht werden!',
+            'delete_button' => 'Benutzer löschen',
+        ],
+    ],
+    /*
+ * Copyright (c) 2021 by HerrTxbias.
+ *
+ * Using / Editing this without my consent is not allowed.
+ */
+    /*
+    |--------------------------------------------------------------------------
+    | Users Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | Please do not edit these lines, as they are managed by our translation
+    | system. Thank you.
+    |
+    */
+    'title' => 'Benutzer',
+];
