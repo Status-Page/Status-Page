c484dd43801f194ff01715100c07406587dc2ee4
HerrTxbias <admin@herrtxbias.net>
2021-03-14 15:55:53 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (6 of 6 strings)
    
    Translation: Status Page/Status
    Translate-URL: http://translate.herrtxbias.net/projects/status-page/status/de/

--- resources/lang/de/status.php
+++ resources/lang/de/status.php
@@ -1,1 +1,23 @@
 <?php
+return [
+    'Operational' => 'Funktionsfähig',
+    'Degraded Performance' => 'Eingeschränkte Leistung',
+    'Partial Outage' => 'Teilweiser Ausfall',
+    'Major Outage' => 'Kompletter Ausfall',
+    'Maintenance' => 'Wartung',
+    /*
+ * Copyright (c) 2021 by HerrTxbias.
+ *
+ * Using / Editing this without my consent is not allowed.
+ */
+    /*
+    |--------------------------------------------------------------------------
+    | Status Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | Please do not edit these lines, as they are managed by our translation
+    | system. Thank you.
+    |
+    */
+    'Unknown' => 'Unbekannt',
+];
