dce45d486b872e2cc6d718424739a15996a9e565
HerrTxbias <admin@herrtxbias.net>
2021-03-25 18:56:10 +0100

    feat(i18n): Add Settings translation Strings

---
+++ resources/lang/en/settings.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ * Copyright (c) 2021 by HerrTxbias.
+ *
+ * Using / Editing this without my consent is not allowed.
+ */
+
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Status Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | Please do not edit these lines, as they are managed by our translation
+    | system. Thank you.
+    |
+    */
+
+    'actionlog_backlog' => 'ActionLog Backlog',
+    'darkmode_default' => 'Darkmode Default Value',
+    'footer_show' => 'Show Footer',
+    'footer_showDashboardLink' => 'Show Link to Dashboard in the Footer',
+    'incidents_pastIncidentDays' => 'Incident Backlog on the Mainpage',
+
+];
