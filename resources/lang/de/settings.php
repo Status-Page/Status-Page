2ee6a9cf825f0bb3f0f4999613a1a6f153282043
HerrTxbias <admin@herrtxbias.net>
2021-03-25 17:58:41 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (5 of 5 strings)
    
    Translation: Status Page/Settings
    Translate-URL: http://translate.herrtxbias.net/projects/status-page/settings/de/

--- resources/lang/de/settings.php
+++ resources/lang/de/settings.php
@@ -1,1 +1,22 @@
 <?php
+return [
+    'darkmode_default' => 'Darkmode Standardwert',
+    'footer_show' => 'Fußzeile anzeigen',
+    'footer_showDashboardLink' => 'Link zum Dashboard in der Fußzeile anzeigen',
+    'incidents_pastIncidentDays' => 'Incident Backlog auf der Hauptseite',
+    /*
+ * Copyright (c) 2021 by HerrTxbias.
+ *
+ * Using / Editing this without my consent is not allowed.
+ */
+    /*
+    |--------------------------------------------------------------------------
+    | Status Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | Please do not edit these lines, as they are managed by our translation
+    | system. Thank you.
+    |
+    */
+    'actionlog_backlog' => 'ActionLog Backlog',
+];
