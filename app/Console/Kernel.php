ad2644b78543df4c8f994b9a2724b28095e99fdf
HerrTxbias <admin@herrtxbias.net>
2021-03-26 01:19:35 +0100

    fix(metrics): Possible fix for Overlapping Cache Jobs

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,6 +7,7 @@
 
 namespace App\Console;
 
+use App\Jobs\CacheMetricData;
 use App\Models\Metric;
 use App\Models\Setting;
 use Cache;
@@ -44,50 +45,10 @@ class Kernel extends ConsoleKernel
                 throw new ProcessFailedException($fetch);
             }
         })->everyFifteenMinutes()->description('Checks for a newer version.');
-        $schedule->call(function (){
-            if(Setting::getBoolean('metrics_cache')){
-                $metrics = Metric::all();
-                foreach ($metrics as $metric){
-                    Cache::put('metric_'.$metric->id.'_24_60', $metric->getIntervalPointsLastHours(24));
-                    Cache::put('metric_'.$metric->id.'_24_30', $metric->getIntervalPointsLastHours(24, 30));
-                    Cache::put('metric_'.$metric->id.'_24_15', $metric->getIntervalPointsLastHours(24, 15));
-                    Cache::put('metric_'.$metric->id.'_24_5', $metric->getIntervalPointsLastHours(24, 5));
-                }
-            }
-        })->everyFiveMinutes()->description('Caches Metric Data (24 Hours)')->runInBackground();
-        $schedule->call(function (){
-            if(Setting::getBoolean('metrics_cache')){
-                $metrics = Metric::all();
-                foreach ($metrics as $metric){
-                    Cache::put('metric_'.$metric->id.'_48_60', $metric->getIntervalPointsLastHours(48));
-                    Cache::put('metric_'.$metric->id.'_48_30', $metric->getIntervalPointsLastHours(48, 30));
-                    Cache::put('metric_'.$metric->id.'_48_15', $metric->getIntervalPointsLastHours(48, 15));
-                    Cache::put('metric_'.$metric->id.'_48_5', $metric->getIntervalPointsLastHours(48, 5));
-                }
-            }
-        })->everyFiveMinutes()->description('Caches Metric Data (48 Hours)')->runInBackground();
-        $schedule->call(function (){
-            if(Setting::getBoolean('metrics_cache')){
-                $metrics = Metric::all();
-                foreach ($metrics as $metric){
-                    Cache::put('metric_'.$metric->id.'_72_60', $metric->getIntervalPointsLastHours(72));
-                    Cache::put('metric_'.$metric->id.'_72_30', $metric->getIntervalPointsLastHours(72, 30));
-                    Cache::put('metric_'.$metric->id.'_72_15', $metric->getIntervalPointsLastHours(72, 15));
-                    Cache::put('metric_'.$metric->id.'_72_5', $metric->getIntervalPointsLastHours(72, 5));
-                }
-            }
-        })->everyFiveMinutes()->description('Caches Metric Data (72 Hours)')->runInBackground();
-        $schedule->call(function (){
-            if(Setting::getBoolean('metrics_cache')){
-                $metrics = Metric::all();
-                foreach ($metrics as $metric){
-                    Cache::put('metric_'.$metric->id.'_168_60', $metric->getIntervalPointsLastHours(168));
-                    Cache::put('metric_'.$metric->id.'_168_30', $metric->getIntervalPointsLastHours(168, 30));
-                    Cache::put('metric_'.$metric->id.'_168_15', $metric->getIntervalPointsLastHours(168, 15));
-                    Cache::put('metric_'.$metric->id.'_168_5', $metric->getIntervalPointsLastHours(168, 5));
-                }
-            }
-        })->everyFiveMinutes()->description('Caches Metric Data (168 Hours)')->runInBackground();
+        $schedule->job(new CacheMetricData(24))->everyFiveMinutes()->description('Caches Metric Data (24 Hours)')->withoutOverlapping();
+        $schedule->job(new CacheMetricData(48))->everyFiveMinutes()->description('Caches Metric Data (48 Hours)')->withoutOverlapping();
+        $schedule->job(new CacheMetricData(72))->everyFiveMinutes()->description('Caches Metric Data (72 Hours)')->withoutOverlapping();
+        $schedule->job(new CacheMetricData(168))->everyFiveMinutes()->description('Caches Metric Data (168 Hours)')->withoutOverlapping();
     }
 
     /**
---
+++ app/Jobs/CacheMetricData.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Metric;
+use App\Models\Setting;
+use Cache;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class CacheMetricData implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    private int $lastHours;
+
+    /**
+     * Create a new job instance.
+     *
+     * @param int $lastHours
+     */
+    public function __construct(int $lastHours)
+    {
+        $this->lastHours = $lastHours;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        if(Setting::getBoolean('metrics_cache')){
+            $metrics = Metric::all();
+            foreach ($metrics as $metric){
+                Cache::put('metric_'.$metric->id.'_'.$this->lastHours.'_60', $metric->getIntervalPointsLastHours($this->lastHours));
+                Cache::put('metric_'.$metric->id.'_'.$this->lastHours.'_30', $metric->getIntervalPointsLastHours($this->lastHours, 30));
+                Cache::put('metric_'.$metric->id.'_'.$this->lastHours.'_15', $metric->getIntervalPointsLastHours($this->lastHours, 15));
+                Cache::put('metric_'.$metric->id.'_'.$this->lastHours.'_5', $metric->getIntervalPointsLastHours($this->lastHours, 5));
+            }
+        }
+    }
+}
