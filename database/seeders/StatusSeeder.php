d6a7cc410247ce48e803f04da25123a4b3c14915
HerrTxbias <admin@herrtxbias.net>
2021-04-09 21:28:06 +0200

    fix(Statuses): Update Colors for better contrast

--- database/seeders/StatusSeeder.php
+++ database/seeders/StatusSeeder.php
@@ -63,8 +63,8 @@ class StatusSeeder extends Seeder
             'order' => '2',
             'name' => 'Degraded Performance',
             'long_description' => 'Some services have performance issues',
-            'color' => 'text-yellow-400',
-            'bg_color' => 'bg-yellow-100',
+            'color' => 'text-yellow-500',
+            'bg_color' => 'bg-yellow-200',
             'border_color' => 'border-yellow-400',
             'heroicon_svg' => '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM7 9a1 1 0 000 2h6a1 1 0 100-2H7z" clip-rule="evenodd" /></svg>',
         ]);
