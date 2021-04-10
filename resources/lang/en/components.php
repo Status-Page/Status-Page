a57ddc79ee6dd688d62fcfc37fa09d1c0fdd95e8
HerrTxbias <admin@herrtxbias.net>
2021-04-10 22:48:25 +0200

    feat(i18n): Add Component language strings

---
+++ resources/lang/en/components.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * Copyright (c) 2021 by HerrTxbias.
+ *
+ * Using / Editing this without my consent is not allowed.
+ */
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Components Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | Please do not edit these lines, as they are managed by our translation
+    | system. Thank you.
+    |
+    */
+
+    'title' => 'Components',
+
+];
--- resources/views/livewire/dashboard/components/components.blade.php
+++ resources/views/livewire/dashboard/components/components.blade.php
@@ -1,7 +1,7 @@
 <div>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-white leading-tight">
-            {{ __('Components') }}
+            {{ __('components.title') }}
         </h2>
     </x-slot>
 
