1685d7d88709d41705bb567c2f5f62a37c3695d1
HerrTxbias <admin@herrtxbias.net>
2021-04-10 00:24:22 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (16 of 16 strings)
    
    Translation: Status Page/Uptimerobot
    Translate-URL: http://translate.herrtxbias.net/projects/status-page/uptimerobot/de/

--- resources/lang/de/uptimerobot.php
+++ resources/lang/de/uptimerobot.php
@@ -1,1 +1,43 @@
 <?php
+return [
+    'title' => 'Monitore',
+    /*
+ * Copyright (c) 2021 by HerrTxbias.
+ *
+ * Using / Editing this without my consent is not allowed.
+ */
+    /*
+    |--------------------------------------------------------------------------
+    | UptimeRobot Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | Please do not edit these lines, as they are managed by our translation
+    | system. Thank you.
+    |
+    */
+    'title_prefix' => 'Uptime-Robot',
+    'table' => [
+        'head' => [
+            'id' => 'ID',
+            'monitor_id' => 'Monitor-ID',
+            'name' => 'Name',
+            'component' => 'Komponente',
+            'metric' => 'Metrik',
+            'data_import' => 'Daten-Import',
+        ],
+        'body' => [
+            'data_import_active' => 'Aktiv',
+            'data_import_paused' => 'Pausiert',
+            'actions' => [
+                'update' => 'Update',
+            ],
+        ],
+    ],
+    'modal_update' => [
+        'title' => 'Monitor aktualisieren',
+        'component' => 'Komponente',
+        'metric' => 'Metrik',
+        'submit' => 'Update',
+    ],
+    'subtitle' => 'Hinweis: Die hier angezeigten Daten werden jede Minute aktualisiert.',
+];
