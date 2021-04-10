bad34431cac615dc4e1050d606d813a0e3638b63
HerrTxbias <admin@herrtxbias.net>
2021-04-10 20:27:52 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (12 of 12 strings)
    
    Translation: Status Page/Home
    Translate-URL: http://translate.herrtxbias.net/projects/status-page/home/de/

--- resources/lang/de/home.php
+++ resources/lang/de/home.php
@@ -1,1 +1,35 @@
 <?php
+return [
+    'metrics' => [
+        'minutes' => ':value Minute|:value Minuten',
+        'title' => 'Metriken',
+        'interval' => 'Intervall',
+        'hours' => ':value Stunde|:value Stunden',
+        'last' => 'Letzte',
+        'day' => ':value Tag|:value Tage',
+    ],
+    'upcoming_maintenances' => [
+        'title' => 'Planmäßige Wartung',
+        'scheduled_for' => 'Geplant für',
+        'affected_components' => 'Betroffene Komponenten',
+    ],
+    'past_incidents' => [
+        'title' => 'Vergangene Vorfälle',
+        'no_incidents' => 'Keine Vorkommnisse gemeldet.',
+    ],
+    /*
+ * Copyright (c) 2021 by HerrTxbias.
+ *
+ * Using / Editing this without my consent is not allowed.
+ */
+    /*
+    |--------------------------------------------------------------------------
+    | Home Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | Please do not edit these lines, as they are managed by our translation
+    | system. Thank you.
+    |
+    */
+    'open_dashboard' => 'Dashboard öffnen',
+];
