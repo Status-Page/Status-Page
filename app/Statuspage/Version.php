00578b95fda70f4f192b6252f1dd272b403cd614
HerrTxbias <admin@herrtxbias.net>
2021-04-10 02:29:00 +0200

    Release v1.6.0

--- app/Statuspage/Version.php
+++ app/Statuspage/Version.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Http;
 class Version
 {
     public static function getVersion(){
-        return 'v1.5.4';
+        return 'v1.6.0';
     }
 
     public static function getLatestVersion() {
