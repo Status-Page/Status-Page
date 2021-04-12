504003729ca4fefb1f760feb8eb6cd6f60b5e50d
HerrTxbias <admin@herrtxbias.net>
2021-04-12 19:38:28 +0200

    update(Settings): Remove metrics_cache from Settings

--- resources/lang/en/settings.php
+++ resources/lang/en/settings.php
@@ -23,7 +23,6 @@ return [
     'footer_show' => 'Show Footer',
     'footer_showDashboardLink' => 'Show Link to Dashboard in the Footer',
     'incidents_pastIncidentDays' => 'Incident Backlog on the Mainpage',
-    'metrics_cache' => 'Cache Metric Data',
     'uptimerobot_key' => 'Uptime Robot Read Only API Key',
     'external_cron' => 'Allow schedule:run command externally'
 
