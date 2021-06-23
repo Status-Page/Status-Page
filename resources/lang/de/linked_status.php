d5dbcfa5e0f4ba4161e62d9ad61e5c9bdf507721
HerrTxbias <admin@herrtxbias.net>
2021-06-23 19:40:15 +0000

    Translated using Weblate (German)
    Currently translated at 10.7% (3 of 28 strings)
    
    Translation: Status Page/Linked_Status
    Translate-URL: http://translate.herrtxbias.net/projects/status-page/linked_status/de/

--- resources/lang/de/linked_status.php
+++ resources/lang/de/linked_status.php
@@ -1,1 +1,20 @@
 <?php
+return [
+    /*
+ * Copyright (c) 2021 by HerrTxbias.
+ *
+ * Using / Editing this without my consent is not allowed.
+ */
+    /*
+    |--------------------------------------------------------------------------
+    | LinkedStatus Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | Please do not edit these lines, as they are managed by our translation
+    | system. Thank you.
+    |
+    */
+    'title_prefix' => 'Verknüpfter Status',
+    'title' => 'Seiten',
+    'subtitle' => 'Fügen Sie externe Statusseiten hinzu, um Komponenten / Vorfälle / Wartungen mit ihnen zu verbinden.',
+];
