5fa526dcf83345ae7e809b318300d456bc8a9692
HerrTxbias <admin@herrtxbias.net>
2021-06-29 00:36:04 +0200

    Bump Version

--- app/Statuspage/Version.php
+++ app/Statuspage/Version.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Http;
 class Version
 {
     public static function getVersion(){
-        return 'v1.8.4';
+        return 'v1.8.5';
     }
 
     public static function getLatestVersion() {
