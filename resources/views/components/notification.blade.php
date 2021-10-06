d36a41203dabe63ae6d1f5160d85539cff2a9ae0
HerrTxbias <admin@herrtxbias.net>
2021-10-06 21:42:36 +0200

    fix: add styling for links in markdown

--- resources/views/components/notification.blade.php
+++ resources/views/components/notification.blade.php
@@ -1,4 +1,4 @@
-<div class="fixed inset-0 flex items-end justify-center px-4 py-6 pointer-events-none sm:p-6 sm:items-start sm:justify-end">
+<!-- <div class="fixed inset-0 flex items-end justify-center px-4 py-6 pointer-events-none sm:p-6 sm:items-start sm:justify-end">
     <div
         x-data="{ show: false, message: '' }"
         x-on:notify.window="show = true; message = $event.detail; setTimeout(() => { show = false }, 2500)"
@@ -35,3 +35,4 @@
         </div>
     </div>
 </div>
+-->
