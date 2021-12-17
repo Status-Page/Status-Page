2a870bb96bfbafcd44e1c92998e2c27a674b739e
Tobias Peltzer <16502676+HerrTxbias@users.noreply.github.com>
2021-12-17 18:31:15 +0100

    New translations settings.php (German)

--- resources/lang/de/settings.php
+++ resources/lang/de/settings.php
@@ -1,41 +1,31 @@
 <?php
+/*
+ * Copyright (c) 2021 by HerrTxbias.
+ *
+ * Using / Editing this without my consent is not allowed.
+ */
+
+
 return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Status Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | Please do not edit these lines, as they are managed by our translation
+    | system. Thank you.
+    |
+    */
+
+    'title' => 'Einstellungen',
+    'actionlog_backlog' => 'ActionLog Backlog',
     'darkmode_default' => 'Darkmode Standardwert',
     'footer_show' => 'Fußzeile anzeigen',
     'footer_showDashboardLink' => 'Link zum Dashboard in der Fußzeile anzeigen',
     'incidents_pastIncidentDays' => 'Incident Backlog auf der Hauptseite',
-    /*
- * Copyright (c) 2021 by HerrTxbias.
- *
- * Using / Editing this without my consent is not allowed.
- */
-    /*
-    |--------------------------------------------------------------------------
-    | Status Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | Please do not edit these lines, as they are managed by our translation
-    | system. Thank you.
-    |
-    */
-    'actionlog_backlog' => 'ActionLog Backlog',
-    'metrics_cache' => 'Metrik-Daten zwischenspeichern',
     'uptimerobot_key' => 'Uptime Robot Schreibgeschützter API-Schlüssel',
     'external_cron' => 'Befehlsausführung von schedule:run von außen zulassen',
-    'subscriber_signup' => 'Erlauben Sie Benutzern, per E-Mail zu abonnieren',
-    /*
- * Copyright (c) 2021 by HerrTxbias.
- *
- * Using / Editing this without my consent is not allowed.
- */
-    /*
-    |--------------------------------------------------------------------------
-    | Status Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | Please do not edit these lines, as they are managed by our translation
-    | system. Thank you.
-    |
-    */
-    'title' => 'Einstellungen',
+    'subscriber_signup' => 'Erlauben Sie Benutzern, per E-Mail zu abonnieren'
+
 ];
