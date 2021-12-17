1d6352cb02035418bb1f17275c27533c9d951afc
Tobias Peltzer <16502676+HerrTxbias@users.noreply.github.com>
2021-12-17 18:31:19 +0100

    New translations users.php (German)

--- resources/lang/de/users.php
+++ resources/lang/de/users.php
@@ -1,5 +1,24 @@
 <?php
+/*
+ * Copyright (c) 2021 by HerrTxbias.
+ *
+ * Using / Editing this without my consent is not allowed.
+ */
+
+
 return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Users Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | Please do not edit these lines, as they are managed by our translation
+    | system. Thank you.
+    |
+    */
+
+    'title' => 'Benutzer',
     'table' => [
         'head' => [
             'name' => 'Name',
@@ -11,28 +30,28 @@ return [
     'new_user' => [
         'button' => 'Benutzer hinzufügen',
         'modal' => [
+            'title' => 'Benutzer hinzufügen',
             'name' => 'Name',
             'email' => 'E-Mail',
             'password' => 'Passwort',
             'deactivated' => 'Deaktiviert',
             'role' => 'Rolle',
             'save_button' => 'Speichern',
-            'title' => 'Benutzer hinzufügen',
-        ],
+        ]
     ],
     'update_user' => [
         'button' => 'Update',
         'modal' => [
             'title' => 'Benutzer Aktualisieren',
+            'name' => 'Name',
+            'email' => 'E-Mail',
             'email_hint' => 'Nur lesen',
             'password' => 'Passwort',
             'password_hint' => 'Wird nur aktualisiert, wenn Sie einen Wert einstellen.',
             'deactivated' => 'Deaktiviert',
             'role' => 'Rolle',
             'update_button' => 'Update',
-            'name' => 'Name',
-            'email' => 'E-Mail',
-        ],
+        ]
     ],
     'delete_user' => [
         'button' => 'Löschen',
@@ -41,21 +60,7 @@ return [
             'text_r1' => 'Sind Sie sicher, dass Sie den Benutzer ":name" löschen wollen?',
             'text_r2' => 'Dies kann nicht rückgängig gemacht werden!',
             'delete_button' => 'Benutzer löschen',
-        ],
+        ]
     ],
-    /*
- * Copyright (c) 2021 by HerrTxbias.
- *
- * Using / Editing this without my consent is not allowed.
- */
-    /*
-    |--------------------------------------------------------------------------
-    | Users Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | Please do not edit these lines, as they are managed by our translation
-    | system. Thank you.
-    |
-    */
-    'title' => 'Benutzer',
+
 ];
