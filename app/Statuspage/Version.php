f8ccb8063d3839b6e020b8c60d22d3e8c2c99bcc
Tobias Peltzer <admin@herrtxbias.net>
2022-01-16 15:33:30 +0100

    chore: Release v1.9.4

--- app/Statuspage/Version.php
+++ app/Statuspage/Version.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Http;
 class Version
 {
     public static function getVersion(){
-        return 'v1.9.3';
+        return 'v1.9.4';
     }
 
     public static function getLatestVersion() {
