5129989f74590c98b21ab525b8a51a4dc830abd7
Tobias Peltzer <admin@herrtxbias.net>
2022-04-27 22:54:52 +0200

    feat: customization of statuses

--- resources/views/vendor/jetstream/components/form-section.blade.php
+++ resources/views/vendor/jetstream/components/form-section.blade.php
@@ -9,7 +9,7 @@
     <div class="mt-5 md:mt-0 md:col-span-2">
         <form wire:submit.prevent="{{ $submit }}">
             <div class="px-4 py-5 bg-white dark:bg-discordDark sm:p-6 shadow {{ isset($actions) ? 'sm:rounded-tl-md sm:rounded-tr-md' : 'sm:rounded-md' }}">
-                <div class="grid grid-cols-6 gap-6">
+                <div class="flex flex-col space-y-6">
                     {{ $form }}
                 </div>
             </div>
