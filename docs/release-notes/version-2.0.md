f1a7acd0a0b76ab0fa39ab0cd441a30ddc8f5edd
Tobias Peltzer <admin@herrtxbias.net>
2022-11-03 14:42:46 +0100

    chore: prepare

--- docs/release-notes/version-2.0.md
+++ docs/release-notes/version-2.0.md
@@ -1,5 +1,9 @@
 # Status-Page v2.0
 
+## v2.0.6 (FUTURE)
+
+---
+
 ## v2.0.5 (2022-11-03)
 
 ### Bug fixes
--- statuspage/statuspage/settings.py
+++ statuspage/statuspage/settings.py
@@ -7,7 +7,7 @@ from django.core.exceptions import ImproperlyConfigured
 
 from statuspage.config import PARAMS
 
-VERSION = '2.0.5'
+VERSION = '2.0.6-dev'
 
 HOSTNAME = platform.node()
 
