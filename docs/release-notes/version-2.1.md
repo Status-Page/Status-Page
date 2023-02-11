12c3ffe9df540b95203bc6e8508fbf373dd19a5e
Tobias Peltzer <admin@herrtxbias.net>
2023-02-11 22:46:39 +0100

    chore: release v2.1.0

--- docs/release-notes/version-2.1.md
+++ docs/release-notes/version-2.1.md
@@ -1,4 +1,4 @@
 # Status-Page v2.1
 
-## v2.1.0 (FUTURE)
+## v2.1.0 (2023-02-11)
 * Add bars to components that show incidents for the past 90 days
--- statuspage/statuspage/settings.py
+++ statuspage/statuspage/settings.py
@@ -7,7 +7,7 @@ from django.core.exceptions import ImproperlyConfigured
 
 from statuspage.config import PARAMS
 
-VERSION = '2.1.0-dev'
+VERSION = '2.1.0'
 
 HOSTNAME = platform.node()
 
