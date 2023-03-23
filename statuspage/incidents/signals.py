22a0db71f9de413165cca3e6381624b086f68314
Tobias Peltzer <admin@herrtxbias.net>
2023-03-23 10:54:01 +0100

    chore: debug

--- statuspage/incidents/signals.py
+++ statuspage/incidents/signals.py
@@ -1,3 +1,5 @@
+import logging
+
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -8,14 +10,15 @@ from utilities.utils import on_transaction_commit, get_mail_domain
 
 @receiver(post_save, sender=Incident)
 @on_transaction_commit
-def send_notifications(sender, instance: Incident, **kwargs):
+def send_incident_notifications(sender, instance: Incident, **kwargs):
+    logger = logging.getLogger('statuspage.incidents.signals')
     is_new = kwargs.get('created', False)
 
     if is_new and instance.visibility:
-        try:
-            subscribers = Subscriber.objects.filter(incident_subscriptions=True)
+        subscribers = Subscriber.objects.filter(incident_subscriptions=True)
 
-            for subscriber in subscribers:
+        for subscriber in subscribers:
+            try:
                 if subscriber.incident_notifications_subscribed_only and len(instance.components.filter(subscribers__in=[subscriber])) == 0:
                     continue
                 subscriber.send_mail(subject=f'Incident - {instance.title}', template='incidents/created', context={
@@ -24,29 +27,30 @@ def send_notifications(sender, instance: Incident, **kwargs):
                 }, headers={
                     'Message-ID': f'incident-{instance.id}-0@{get_mail_domain()}',
                 })
-        except Exception as e:
-            print(e)
-            pass
+            except Exception as e:
+                logger.error(e)
+                pass
 
 
 @receiver(post_save, sender=IncidentUpdate)
 @on_transaction_commit
-def send_notifications(sender, instance: IncidentUpdate, **kwargs):
+def send_incident_update_notifications(sender, instance: IncidentUpdate, **kwargs):
+    logger = logging.getLogger('statuspage.incidents.signals')
     is_new = kwargs.get('created', False)
 
     if is_new and instance.incident.visibility:
-        try:
-            subscribers = Subscriber.objects.filter(incident_subscriptions=True)
-            message_id = f'incident-{instance.incident.id}-{instance.id}@{get_mail_domain()}'
-            previous_message_ids = [
-                f'incident-{instance.incident.id}-0@{get_mail_domain()}',
-                *list(map(
-                    lambda update: f'incident-{instance.incident.id}-{update.id}@{get_mail_domain()}',
-                    instance.incident.updates.all()
-                ))
-            ]
+        subscribers = Subscriber.objects.filter(incident_subscriptions=True)
+        message_id = f'incident-{instance.incident.id}-{instance.id}@{get_mail_domain()}'
+        previous_message_ids = [
+            f'incident-{instance.incident.id}-0@{get_mail_domain()}',
+            *list(map(
+                lambda update: f'incident-{instance.incident.id}-{update.id}@{get_mail_domain()}',
+                instance.incident.updates.all()
+            ))
+        ]
 
-            for subscriber in subscribers:
+        for subscriber in subscribers:
+            try:
                 if subscriber.incident_notifications_subscribed_only and len(instance.incident.components.filter(subscribers__in=[subscriber])) == 0:
                     continue
                 subscriber.send_mail(subject=f'Incident - {instance.incident.title}', template='incidentupdates/created', context={
@@ -57,6 +61,6 @@ def send_notifications(sender, instance: IncidentUpdate, **kwargs):
                     'Message-ID': message_id,
                     'References': ' '.join(previous_message_ids),
                 })
-        except Exception as e:
-            print(e)
-            pass
+            except Exception as e:
+                logger.error(e)
+                pass
