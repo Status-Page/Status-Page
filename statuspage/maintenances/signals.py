0f54e6bdea60edd4a380ef0941d4ff962165d07b
Tobias Peltzer <admin@herrtxbias.net>
2023-03-23 10:57:19 +0100

    fix: method names

--- statuspage/maintenances/signals.py
+++ statuspage/maintenances/signals.py
@@ -1,3 +1,5 @@
+import logging
+
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -8,14 +10,15 @@ from utilities.utils import on_transaction_commit, get_mail_domain
 
 @receiver(post_save, sender=Maintenance)
 @on_transaction_commit
-def send_notifications(sender, instance: Maintenance, **kwargs):
+def send_maintenance_notifications(sender, instance: Maintenance, **kwargs):
+    logger = logging.getLogger('statuspage.maintenances.signals')
     is_new = kwargs.get('created', False)
 
     if is_new and instance.visibility:
-        try:
-            subscribers = Subscriber.objects.filter(incident_subscriptions=True)
+        subscribers = Subscriber.objects.filter(incident_subscriptions=True)
 
-            for subscriber in subscribers:
+        for subscriber in subscribers:
+            try:
                 if subscriber.incident_notifications_subscribed_only and len(instance.components.filter(subscribers__in=[subscriber])) == 0:
                     continue
                 subscriber.send_mail(subject=f'Maintenance - {instance.title}', template='maintenances/created', context={
@@ -24,28 +27,30 @@ def send_notifications(sender, instance: Maintenance, **kwargs):
                 }, headers={
                     'Message-ID': f'maintenance-{instance.id}-0@{get_mail_domain()}',
                 })
-        except:
-            pass
+            except Exception as e:
+                logger.error(e)
+                pass
 
 
 @receiver(post_save, sender=MaintenanceUpdate)
 @on_transaction_commit
-def send_notifications(sender, instance: MaintenanceUpdate, **kwargs):
+def send_maintenance_update_notifications(sender, instance: MaintenanceUpdate, **kwargs):
+    logger = logging.getLogger('statuspage.maintenances.signals')
     is_new = kwargs.get('created', False)
 
     if is_new and instance.maintenance.visibility:
-        try:
-            subscribers = Subscriber.objects.filter(incident_subscriptions=True)
-            message_id = f'maintenance-{instance.maintenance.id}-{instance.id}@{get_mail_domain()}'
-            previous_message_ids = [
-                f'maintenance-{instance.maintenance.id}-0@{get_mail_domain()}',
-                *list(map(
-                    lambda update: f'maintenance-{instance.maintenance.id}-{update.id}@{get_mail_domain()}',
-                    instance.maintenance.updates.all()
-                ))
-            ]
+        subscribers = Subscriber.objects.filter(incident_subscriptions=True)
+        message_id = f'maintenance-{instance.maintenance.id}-{instance.id}@{get_mail_domain()}'
+        previous_message_ids = [
+            f'maintenance-{instance.maintenance.id}-0@{get_mail_domain()}',
+            *list(map(
+                lambda update: f'maintenance-{instance.maintenance.id}-{update.id}@{get_mail_domain()}',
+                instance.maintenance.updates.all()
+            ))
+        ]
 
-            for subscriber in subscribers:
+        for subscriber in subscribers:
+            try:
                 if subscriber.incident_notifications_subscribed_only and len(instance.maintenance.components.filter(subscribers__in=[subscriber])) == 0:
                     continue
                 subscriber.send_mail(subject=f'Maintenance - {instance.maintenance.title}', template='maintenanceupdates/created', context={
@@ -56,5 +61,6 @@ def send_notifications(sender, instance: MaintenanceUpdate, **kwargs):
                     'Message-ID': message_id,
                     'References': ' '.join(previous_message_ids),
                 })
-        except:
-            pass
+            except Exception as e:
+                logger.error(e)
+                pass
