1c19a0f7b08fe6da41230d21d07e052508c740ef
Tobias Peltzer <admin@herrtxbias.net>
2023-06-04 23:50:40 +0200

    refactor: migrate sp_uptimerobot to registry

--- statuspage/sp_uptimerobot/urls.py
+++ statuspage/sp_uptimerobot/urls.py
@@ -1,17 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 
-from statuspage.views.generic import ObjectChangeLogView
-from . import views
-from .models import UptimeRobotMonitor
+from utilities.urls import get_model_urls
+from . import views # noqa Required for registration
 
 urlpatterns = [
-    path('', views.UptimeRobotMonitorListView.as_view(), name='uptimerobotmonitor_list'),
-    path('edit/', views.UptimeRobotMonitorBulkEditView.as_view(), name='uptimerobotmonitor_bulk_edit'),
-    path('delete/', views.UptimeRobotMonitorBulkDeleteView.as_view(), name='uptimerobotmonitor_bulk_delete'),
-    path('<int:pk>/', views.UptimeRobotMonitorView.as_view(), name='uptimerobotmonitor'),
-    path('<int:pk>/edit/', views.UptimeRobotMonitorEditView.as_view(), name='uptimerobotmonitor_edit'),
-    path('<int:pk>/delete/', views.UptimeRobotMonitorDeleteView.as_view(), name='uptimerobotmonitor_delete'),
-    path('<int:pk>/changelog/', ObjectChangeLogView.as_view(), name='uptimerobotmonitor_changelog', kwargs={
-        'model': UptimeRobotMonitor,
-    }),
+    path('', include(get_model_urls('sp_uptimerobot', 'uptimerobotmonitor'))),
 ]
