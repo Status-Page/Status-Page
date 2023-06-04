2b713b5e86967b487c64abe23e40e2acec52dacf
Tobias Peltzer <admin@herrtxbias.net>
2023-06-04 22:59:38 +0200

    refactor: migrate components to registry

--- statuspage/components/urls.py
+++ statuspage/components/urls.py
@@ -1,30 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 
-from statuspage.views.generic import ObjectChangeLogView
-from . import views
-from .models import Component, ComponentGroup
+from utilities.urls import get_model_urls
+
+from components import views # noqa Required for registration
 
 app_name = 'components'
 urlpatterns = [
-    path('', views.ComponentListView.as_view(), name='component_list'),
-    path('add/', views.ComponentEditView.as_view(), name='component_add'),
-    path('edit/', views.ComponentBulkEditView.as_view(), name='component_bulk_edit'),
-    path('delete/', views.ComponentBulkDeleteView.as_view(), name='component_bulk_delete'),
-    path('<int:pk>/', views.ComponentView.as_view(), name='component'),
-    path('<int:pk>/edit/', views.ComponentEditView.as_view(), name='component_edit'),
-    path('<int:pk>/delete/', views.ComponentDeleteView.as_view(), name='component_delete'),
-    path('<int:pk>/changelog/', ObjectChangeLogView.as_view(), name='component_changelog', kwargs={
-        'model': Component,
-    }),
-
-    path('groups/', views.ComponentGroupListView.as_view(), name='componentgroup_list'),
-    path('groups/add/', views.ComponentGroupEditView.as_view(), name='componentgroup_add'),
-    path('groups/edit/', views.ComponentGroupBulkEditView.as_view(), name='componentgroup_bulk_edit'),
-    path('groups/delete/', views.ComponentGroupBulkDeleteView.as_view(), name='componentgroup_bulk_delete'),
-    path('groups/<int:pk>/', views.ComponentGroupView.as_view(), name='componentgroup'),
-    path('groups/<int:pk>/edit/', views.ComponentGroupEditView.as_view(), name='componentgroup_edit'),
-    path('groups/<int:pk>/delete/', views.ComponentGroupDeleteView.as_view(), name='componentgroup_delete'),
-    path('groups/<int:pk>/changelog/', ObjectChangeLogView.as_view(), name='componentgroup_changelog', kwargs={
-        'model': ComponentGroup,
-    }),
+    path('', include(get_model_urls('components', 'component'))),
+    path('groups/', include(get_model_urls('components', 'componentgroup'))),
 ]
